52d679d1e112238ec5ff5e9ce3c03ef5a2f3bc13
mitzCanCode <dimitris.chatzi@proton.me>
2025-08-24 01:19:05 +0300

    Initial Commit

---
+++ Anvil/AnvilApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnvilApp.swift
+//  Anvil
+//
+//  Created by Dimitris Chatzigeorgiou on 24/8/25.
+//
+
+import SwiftUI
+
+@main
+struct AnvilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnvilTests/AnvilTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnvilTests.swift
+//  AnvilTests
+//
+//  Created by Dimitris Chatzigeorgiou on 24/8/25.
+//
+
+import Testing
+@testable import Anvil
+
+struct AnvilTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
